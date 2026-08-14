a410bd192772c9b423a1972839003d5cae93bfb7
Logan Barcellos <107962175+Fr3zzy3433@users.noreply.github.com>
2026-08-14 09:15:50 -0300

    chore: keep deterministic test data minimal

--- fixtures/users.js
+++ fixtures/users.js
@@ -3,23 +3,17 @@ const users = {
         username: 'standard_user',
         password: 'secret_sauce'
     },
-
     locked: {
         username: 'locked_out_user',
-        password: 'secret_sauce'
-    },
-
-    problem: {
-        username: 'problem_user',
         password: 'secret_sauce'
     }
 };
 
 const checkoutData = {
     valid: {
-        firstName: 'Marcius',
-        lastName: 'Barcellos',
-        postalCode: '90000-000'
+        firstName: 'QA',
+        lastName: 'Tester',
+        postalCode: '20000-000'
     }
 };
 
